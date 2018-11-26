7fb942cd9f04546b82bcacc6c96eb770493b5f10
Can Colpan <gitlab@cancolpan.com>
2018-11-26 11:53:54 -0600

    Front and back basic route structure completed. Home, Product, Category, Order, Payment, User Controllers created with basic version

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/category/{slug}', 'CategoryController@index')->name('category');
+
+Route::get('/product/{slug}', 'ProductController@index')->name('product');
+
+Route::get('/cart','CartController@index')->name('cart');
+
+Route::get('/order', 'OrderController@index')->name('order');
+
+Route::get('/payment', 'PaymentController@index')->name('payment');
+
+Route::get('/user', 'UserController@index')->name('user');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'],function (){
+
+    Route::resource('/product', 'ProductController');
+    Route::resource('/category', 'CategoryController');
+    Route::get('/order', 'OrderController@index');
+
+
 });
